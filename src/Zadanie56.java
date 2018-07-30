54313aa39c6508d75176276a59f36544ebaa99e5
PC <p.cz@outlook.com>
2018-07-30 16:25:00 +0200

    Zadanie 5.4 i 5.6 poprawione

--- src/Zadanie56.java
+++ src/Zadanie56.java
@@ -11,22 +11,18 @@ public class Zadanie56 {
         int limit2 = 200;
 
         System.out.println("Podaj liczbę");
-        while ((liczba = scan.nextInt()) < 100 || liczba > 200) {
+        while ((liczba = scan.nextInt()) < 100 || liczba > 200 || liczba % 3 != 0) {
             if (liczba < 100) {
                 System.out.println("Podana liczba jest za mała, zgaduj dalej!");
             } else if (liczba > 200) {
                 System.out.println("Podana liczba jest za duża, zgaduj dalej!");
+            } else if (liczba % 3 != 0) {
+                System.out.println("Twoja liczba nie jest podzielna przez 3");
             }
         }
-        if (liczba % 3 != 0) {
-            System.out.println("Twoja liczba nie jest podzielna przez 3");
-        }
-            else
-            System.out.println("Twoja liczba jest spoko!");
-
-            scan.close();
-        }
+        System.out.println("Twoja liczba jest spoko!");
     }
+}
 
 
 
